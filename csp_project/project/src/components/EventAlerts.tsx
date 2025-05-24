c1d18db3f3057665effb61750121a3cd70112dae
AUSPEXI <hopiumcalculator@gmail.com>
2025-05-24 15:35:04 +0100

    Update EventAlerts.tsx

--- csp_project/project/src/components/EventAlerts.tsx
+++ csp_project/project/src/components/EventAlerts.tsx
@@ -6,7 +6,7 @@ const EventAlerts: React.FC = () => {
   return (
     <div className="event-alerts">
       <h2>Crypto News</h2>
-      <div>
+      <div className="iframe-container">
         <iframe
           src="https://cryptopanic.com/widgets/news/?filter=hot¤cies=BTC,ETH,SOL,USDT,BNB,XRP,DOGE,TON,ADA,TRX,AVAX,SHIB,LINK,BCH,DOT,NEAR,LTC,MATIC,PEPE&theme=light"
           width="100%"
