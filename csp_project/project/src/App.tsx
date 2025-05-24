60bc1f8590dad9c3e6b47aef4eeccad242daea0a
AUSPEXI <hopiumcalculator@gmail.com>
2025-05-24 15:52:15 +0100

    Update App.tsx

--- csp_project/project/src/App.tsx
+++ csp_project/project/src/App.tsx
@@ -8,6 +8,7 @@ import PortfolioTracker from './components/PortfolioTracker';
 import Header from './components/Header';
 import CoinSelect from './components/CoinSelect';
 import DatasetInfo from './components/DatasetInfo';
+import About from './components/About';
 
 const App: React.FC = () => {
   const [selectedCoins, setSelectedCoins] = useState<string[]>(['BTC', 'ETH', 'USDT']);
@@ -18,18 +19,25 @@ const App: React.FC = () => {
         <Header />
         <div className="container mx-auto p-4">
           <div className="mb-6">
+            <About />
+          </div>
+          <div className="mb-6">
             <CoinSelect selectedCoins={selectedCoins} onChange={setSelectedCoins} />
           </div>
           <div className="grid grid-cols-1 lg:grid-cols-[2fr,1fr] gap-6">
             <div className="space-y-6">
               <SentimentSnapshot selectedCoins={selectedCoins} />
               <EventAlerts />
-              <PortfolioTracker />
-              <DatasetInfo />
             </div>
             <div className="space-y-6">
               <OnChainInsights selectedCoins={selectedCoins} />
             </div>
+          </div>
+          <div className="mt-6 w-full">
+            <PortfolioTracker />
+          </div>
+          <div className="mt-6 w-full">
+            <DatasetInfo />
           </div>
         </div>
       </div>
